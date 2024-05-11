f09490b9175a9fe54551df2aa79b4eb9e5920973
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-05-11 09:31:32 +0530

    Update txt2img.py

--- modules/txt2img.py
+++ modules/txt2img.py
@@ -6,11 +6,14 @@ import numpy as np
 from PIL import Image
 import gradio as gr
 import torch
+import os
+import subprocess
 from diffusers import DiffusionPipeline
 from modules import pipeline
 from modules import pipeline as pipe_module
 from modules.pipeline import load_pipeline_txt2img
-
+from modules import codeformer_upscale
+from modules.codeformer_upscale import face_upscale_codeformer
 # Define the function that ensures a number is divisible by 8 and close to the input value
 def closest_divisible_by_8(number: int) -> int:
     remainder = number % 8
@@ -24,7 +27,7 @@ def closest_divisible_by_8(number: int) -> int:
     else:
         return higher_nearest
 
-def txt2img(prompt_t2i, negative_prompt_t2i, height_t2i, width_t2i, num_inference_steps_t2i, guidance_scale_t2i, batch_count_t2i, seed_int="", scheduler=None):
+def txt2img(restore_face_t2i, prompt_t2i, negative_prompt_t2i, height_t2i, width_t2i, num_inference_steps_t2i, guidance_scale_t2i, batch_count_t2i, seed_int="", scheduler=None):
     if seed_int == "":
         seed = random.randint(0, sys.maxsize)
     else:
@@ -45,10 +48,29 @@ def txt2img(prompt_t2i, negative_prompt_t2i, height_t2i, width_t2i, num_inferenc
     width_t2i = closest_divisible_by_8(width_t2i)
 
     images = pipe_module.pipeline(prompt=prompt_t2i, negative_prompt=negative_prompt_t2i, height=height_t2i, width=width_t2i, num_inference_steps=num_inference_steps_t2i, guidance_scale=guidance_scale_t2i, num_images_per_prompt=batch_count_t2i).images
+    metadata_str = f" Seed: {seed}, Prompt: {prompt_t2i}, Negative Prompt: {negative_prompt_t2i}, Height: {height_t2i}, Width: {width_t2i}, Num Inference Steps: {num_inference_steps_t2i}, Guidance Scale: {guidance_scale_t2i}"
 
     images_np = [np.array(img) for img in images]
     images_pil = [Image.fromarray(img_np) for img_np in images_np]
 
-    # Construct metadata string
-    metadata_str = f" Seed: {seed}, Prompt: {prompt_t2i}, Negative Prompt: {negative_prompt_t2i}, Height: {height_t2i}, Width: {width_t2i}, Num Inference Steps: {num_inference_steps_t2i}, Guidance Scale: {guidance_scale_t2i}"
-    return images_pil, images_pil, metadata_str
+    # Save the images to the final results folder
+    inputs_dir = '/content/GenX/images/inputs'
+    temp_dir = '/content/GenX/images/temp'
+    os.makedirs(inputs_dir, exist_ok=True)
+    os.makedirs(temp_dir, exist_ok=True)
+
+    for idx, img_pil in enumerate(images_pil):
+        img_pil.save(os.path.join(inputs_dir, f'image_{idx}.png'))
+        img_pil.save(os.path.join(temp_dir, f'image_{idx}.png'))
+
+    # Get the latest file in the final_results directory
+    latest_file = max([os.path.join(temp_dir, f) for f in os.listdir(temp_dir)], key=os.path.getctime)
+
+    # Open the latest image using PIL
+    latest_image = Image.open(latest_file)
+    if restore_face_t2i == True:
+        latest_image = face_upscale_codeformer(latest_image)
+    # Convert to PIL image
+    latest_image_pil = latest_image if isinstance(latest_image, Image.Image) else Image.fromarray(latest_image)
+    
+    return [latest_image_pil], [latest_image_pil], metadata_str
