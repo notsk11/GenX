7731450f1a01859301ab280f01b6aa956aeb3f41
notsk11 <153056264+notsk11@users.noreply.github.com>
2024-05-11 08:16:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ import os
 import subprocess
 import contextlib
 
-if not os.path.exists('/content/StableDIFF/repository/'):
-    os.mkdir('/content/StableDIFF/repository/')
+if not os.path.exists('/content/GenX/repository/'):
+    os.mkdir('/content/GenX/repository/')
 else:
     print("Directory already exists.")
 
-os.chdir('/content/StableDIFF/repository/')
+os.chdir('/content/GenX/repository/')
 
 with contextlib.redirect_stdout(None), contextlib.redirect_stderr(None):
-    result = subprocess.run(['git', 'clone', 'https://github.com/notsk11/CodeFormer', '/content/StableDIFF/repository/CodeFormer/'])
+    result = subprocess.run(['git', 'clone', 'https://github.com/notsk11/CodeFormer', '/content/GenX/repository/CodeFormer/'])
 
 if result.returncode == 0:
     print("Copied CodeFormer Repo")
 else:
     print("Failed to copy CodeFormer Repo")
 
-os.chdir('/content/StableDIFF/repository/CodeFormer/')
+os.chdir('/content/GenX/repository/CodeFormer/')
 
 with contextlib.redirect_stdout(None), contextlib.redirect_stderr(None):
     subprocess.run(['pip', 'install', '-r', 'requirements.txt', '-q'])
@@ -31,9 +31,9 @@ subprocess.run(['python', 'scripts/download_pretrained_models.py', 'facelib'])
 subprocess.run(['python', 'scripts/download_pretrained_models.py', 'CodeFormer'])
 print("Downloaded pre-trained models (facelib and CodeFormer)...")
 
-os.chdir('/content/StableDIFF/')
+os.chdir('/content/GenX/')
 
 with contextlib.redirect_stdout(None), contextlib.redirect_stderr(None):
     subprocess.run(['pip', 'install', '-r', 'requirements.txt', '-q'])
-    print("Installed StableDIFF Requirements")
+    print("Installed GenX Requirements")
 print("Installation and setup complete.")
